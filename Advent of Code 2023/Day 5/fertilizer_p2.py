d365f21a1f2c76f69ce029787d22f9f451dc2aec
PawelSp <PSprawka@adva.com>
2023-12-08 15:10:09 +0100

    Advent of Code Day 5 part 2

--- Advent of Code 2023/Day 5/fertilizer_p2.py
+++ Advent of Code 2023/Day 5/fertilizer_p2.py
@@ -59,18 +59,36 @@ humidity_to_location_map = find_map("humidity-to-location map:")
 
 conversion_maps = [seed_to_soil_map, soil_to_fertilizer_map, fertilizer_to_water_map, water_to_light_map,
                    light_to_temperature_map, temperature_to_humidity_map, humidity_to_location_map]
-# convert seeds to seed ranges:
-seed_ranges = [range(seeds[x],seeds[x] + 5000000) for x in range(0, len(seeds), 2)]
-seed_ranges_lowest_locations = []
-# convert seeds through all maps all the way to location
-for seed_range in seed_ranges:
-    seeds = list(seed_range)
-    #print(seeds)
-    for conversion_map in conversion_maps:
-        seeds = convert_values(seeds, conversion_map)
-    lowest_location_in_seed_range = min(seeds)
-    print(f"{lowest_location_in_seed_range} in {seed_range}")
-    seed_ranges_lowest_locations.append(lowest_location_in_seed_range)
 
-# print lowest location value from all converted seed ranges
-print(min(seed_ranges_lowest_locations))
+# convert seeds to seed ranges:
+seed_ranges = [range(seeds[x], seeds[x] + seeds[x + 1]) for x in range(0, len(seeds), 2)]
+
+# reverse engineering to find best seed from maps:
+# revert maps and their order so they go from location to seed
+reverted_conversion_maps = []
+for conversion_map in conversion_maps[::-1]:
+    reversed_map = []
+    for map_values in conversion_map:
+        reversed_map.append([map_values[1], map_values[0], map_values[2]])
+    reverted_conversion_maps.append(reversed_map)
+
+# Start with location = 0, lookup for corresponding seed, if there's no seed, try location + addition until seed is found
+# Then start again but with addition/10 and until location reaches previous lowest location found
+
+location = 0
+found = False
+while not found:
+    input_seeds = [location]
+    # look for seed based on location
+    for reverted_map in reverted_conversion_maps:
+        input_seeds = convert_values(input_seeds, reverted_map)
+    # look if found seed is in any of given seed ranges
+    for seed_range in seed_ranges:
+        # print(seed_range)
+        if input_seeds[0] in seed_range:
+            print(f"seed found {input_seeds} for location {location} in range {seed_range}")
+            found = True
+            # Update previous location and add it to locations found list
+            previous_location = location
+    else:
+        location += 1
